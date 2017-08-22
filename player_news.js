eb9c16a85807bbad0ab3b07ebc6dc7053e44e785
vbaghdas <vbaghdas@gmail.com>
2017-08-22 13:11:57 -0700

    Added Player News and Styling

---
+++ player_news.js
@@ -0,0 +1,47 @@
+
+$(document).ready(function() {
+
+playerNews = new PlayerNews;
+
+});
+
+function PlayerNews () {
+
+    console.log('this works');
+        $.ajax({
+        url: 'https://newsapi.org/v1/articles?source=nfl-news&sortBy=top&apiKey=3fde3b14410a4ffd9ec5807677972117',
+        dataType: 'json',
+        method: 'get',
+        data: {
+
+        },
+        success: function(response) {
+            for(var i = 0; i < response.articles.length; i++){
+                console.log(response.articles[i]);
+                var div1 = $('<div>',{
+                    text: response.articles[i].title
+                });
+                var div2 = $('<div>',{
+                    text: response.articles[i].description
+                });
+                $('.playerNews').append(div1);
+                $('.playerNews').append(div2);
+                var img = $('<img>',{
+                    src: response.articles[i].urlToImage
+                });
+                $('.playerNews').append(img);
+
+            }
+
+
+        }
+        // error: function(xhr, ajaxOptions, thrownError) {
+        // }
+    })
+}
+
+
+var playerNews = null;
+
+
+
